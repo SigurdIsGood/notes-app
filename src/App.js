a12fd50903e9aee521081e73e7dfb92a8204e656
JonathanG <jongutjuarez@gmail.com>
2022-10-20 00:06:09 -0600

    adds /services/notes to separate backend functionality

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
-  useEffect(() => { 
-    axios
-      .get('http://localhost:3001/notes')
+  useEffect(() => {
+    noteService
+      .getAll()
       .then(response => {
         setNotes(response.data)
       })
   }, [])
 
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important}
+
+    noteService
+      .update(id,changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id !== id ? note : response.data))
+      })
+  }
+
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      date: new Date(),
-      important: Math.random() < 0.5,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5
     }
 
-    axios
-      .post('http://localhost:3001/notes', noteObject)
+    noteService
+      .create(noteObject)
       .then(response => {
         setNotes(notes.concat(response.data))
-        setNewNote('')    
+        setNewNote('')
       })
   }
 
@@ -39,16 +51,6 @@ const App = () => {
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
-
-  const toggleImportanceOf = id => {
-    const url = `http://localhost:3001/notes/${id}`
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: ~note.important}
-
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(n => n.id !== id ? n : response.data))
-    })
-  }
 
   return (
     <div>
